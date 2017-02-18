afc226f0616883cd1ec635492e608884e4451f9f
Yixuan-Angela <liyixuan0612@gmail.com>
2017-02-18 18:43:50 -0500

    postings

--- controllers/postings.js
+++ controllers/postings.js
@@ -3,6 +3,8 @@
  * List all postings.
  */
 const Postings = require('../models/Postings');
+const mongoose = require('mongoose');
+
 
 exports.getPostings = (req, res) => {
   Postings.find((err, docs) => {
@@ -11,10 +13,63 @@ exports.getPostings = (req, res) => {
 };
 
 
-exports.addPostings = (req, res, next) => {
+exports.addPostings = (req, res) => {
 
-	const posting = new Postings({
-		"referenceID" : req.body.referenceID,
+	var PostingSchema = mongoose.model('Postings', PostingSchema)
+
+	console.log(req.body);
+	console.log(req.body.title);
+
+	var Thetitle = req.body.title;
+	if (Thetitle)
+	{
+		console.log("defined title");
+	}
+
+	var newposting = new Postings({
+
+		//generate id
+
+		"title" :req.body.title,
+	  	"email": req.body.email,
+	  	"price": req.body.price,
+	  	"numPeople": req.body.numPeople,
+	  	"postingDate": Date(),
+
+		"adress":
+		{
+			"address": req.body.address,
+		    "city": req.body.city,
+		    "zip": req.body.zip,
+		    "picture": req.body.picture
+		}
+	});
+	
+
+	// if there is a title then save
+	if (req.body.title)
+	{
+		console.log("saving to db");
+
+		newposting.save(function(err){
+			if (err)
+			{
+				console.log(err);
+				//res.redirect('/');
+			}
+			else
+			{
+				//res.redirect('/postings');
+			}
+	  	});
+	}
+
+
+
+	/*
+	new Postings({
+		/*
+		//"referenceID" : req.body.referenceID,
 		"title" :req.body.posttitle,
 	  	"userEmail": req.body.userEmail,
 	  	"price": req.body.price,
@@ -28,13 +83,23 @@ exports.addPostings = (req, res, next) => {
 		    "picture": req.body.picture
 		}
 
-	});
+		
 
-	//posting.save((err) => {
-    // if (err) { return next(err); }
-    //  res.redirect('/');
-    //});
+		"title" : "bogusTitle"
 
+
+	}).save(function(err){
+		if (err)
+		{
+			console.log(err);
+			//res.redirect('/');
+		}
+		else
+		{
+			//res.redirect('/postings');
+		}
+    });
+	*/
 
 	res.render('newpost',
 	{
